534d1ed0c08e44cc0e0cc89b3c8e21795fb51ec3
NurmuhammadNizomov <nurmuhammadnizomov13@gmail.com>
2024-11-14 13:24:41 +0500

    [ADD] debug mongoose

--- src/config/db.ts
+++ src/config/db.ts
@@ -5,6 +5,9 @@ const connectDB = async (): Promise<void> => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI as string);
     logger.info(`MongoDB Connected: ${conn.connection.host}`);
+    if (process.env.NODE_ENV === "development") {
+      mongoose.set("debug", true);
+    }
   } catch (error) {
     logger.error("Error connecting to MongoDB:", error);
     process.exit(1); // Exit process with failure
